bd6006742982b735cc357129c28f268d77204d50
Gabriel Fernandes <gabrielfvp32015@gmail.com>
2023-10-23 20:07:46 -0300

    update

--- ic01/src/pages/register/index.jsx
+++ ic01/src/pages/register/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { auth } from '../../services/firebaseConfig';
+import { auth, db } from '../../services/firebaseConfig';
+import { collection, addDoc } from "firebase/firestore";
 import { useState } from "react";
 import logo from "../../imgs/logo1.png";
 import "./style.css";
@@ -8,18 +9,39 @@ import "./style.css";
 export function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [role, setRole] = useState('');
 
-  const [createUserWithEmailAndPassword, user, loading, error] = 
+  const [createUserWithEmailAndPassword, user, loading, error] =
     useCreateUserWithEmailAndPassword(auth);
 
-    function handleSignOut(e) {
-      e.preventDefault();
-      createUserWithEmailAndPassword(email, password);
-    }
+  function handleSignOut(e) {
+    e.preventDefault();
+    createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        // Store the user data in the respective collection based on the selected role
+        addDoc(collection(db, role), {
+          name: name,
+          email: email,
+          role: role,
+          uid: user.uid
+        })
+        .then(() => {
+          console.log("User data stored successfully");
+        })
+        .catch((error) => {
+          console.error("Error storing user data: ", error);
+        });
+      })
+      .catch((error) => {
+        console.error("Error creating user: ", error);
+      });
+  }
 
-    if (loading) {
-      return <p>carregando...</p>;
-    }
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
 
   return (
     <div className="container">
@@ -30,36 +52,61 @@ export function Register() {
 
       <form>
         <div className="inputContainer">
-          <label htmlFor="email">E-mail</label>
-          <input 
-            type="text" 
-            name="email" 
-            id="email" 
-            placeholder="joao@gmail.com"
-            onChange={(e) => setEmail(e.target.value)}>
-          </input>
-        </div>
-        
-        <div className="inputContainer">
-          <label htmlFor="password">Password</label>
-          <input 
-            type="password" 
-            name="password" 
-            id="password" 
-            placeholder="************"
-            onChange={(e) => setPassword(e.target.value)}>
-          </input>
+          <label htmlFor="name">Nome</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="João da Silva"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-        <button className="conect-button" onClick={handleSignOut}>Cadastrar</button>
+        <div className="inputContainer">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="joao@gmail.com"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="inputContainer">
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="************"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className="inputContainer">
+          <label htmlFor="role">Função</label>
+          <select
+            name="role"
+            id="role"
+            onChange={(e) => setRole(e.target.value)}
+          >
+            <option value="">Selecione a função</option>
+            <option value="Aluno">Aluno</option>
+            <option value="Professor">Professor</option>
+            <option value="Adm">Administrador</option>
+          </select>
+        </div>
+
+        <button className="conect-button" onClick={handleSignOut}>
+          Cadastrar
+        </button>
 
         <div className='footer'>
-          <p>Você já tem uma conta?</p>
+          <p>Já possui uma conta?</p>
           <Link to='/'>Acesse sua conta aqui</Link>
         </div>
-
       </form>
     </div>
   );
 }
-
